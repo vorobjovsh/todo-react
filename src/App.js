72122faab4ec9fc860f122d2217721000a1651af
vorobjovsh <vorobjov75@gmail.com>
2022-07-15 19:13:15 +0300

    add items to the list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
       <h1>React Todo App</h1>
+
+      <div className="input-wrapper">
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          placeholder="Create a new todo"
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <button className="add-button" onClick={addTodo}>
+          Add
+        </button>
+      </div>
     </div>
   );
 };
